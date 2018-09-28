b2b73155d613bb2549dc3b2cea46613eba30d1fb
Mohammed Mudassir Ali <alimudassir12@gmail.com>
2018-09-29 02:02:57 +0530

    Added Project Lombok

--- src/main/java/com/maverick/cookingrecipes/controllers/IndexController.java
+++ src/main/java/com/maverick/cookingrecipes/controllers/IndexController.java
@@ -1,11 +1,14 @@
 package com.maverick.cookingrecipes.controllers;
 
 import com.maverick.cookingrecipes.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
+
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -17,7 +20,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
-
+        log.debug("Loading Index");
         model.addAttribute("recipes", recipeService.getRecipes());
 
         return "index";
